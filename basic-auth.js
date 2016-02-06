f52ec52a5f2592d36db6e67c45fc6c24635cb983
Rami Jarrar <rami@talos.email>
2016-02-06 23:54:40 +1100

    Fix connect handler definition

--- basic-auth.js
+++ basic-auth.js
@@ -1,25 +1,28 @@
 Meteor.startup(function () {
+    var stack;
+
     if (process.env.RESTRICT_ENV) {
         // Ensure credentials are provided
         if (!Meteor.settings.basicAuth) {
             throw new Error("[talos:basic-auth] No credentials were found in your meteor settings");
         }
 
-        // Add connect handler at the top of the stack
-        WebApp.connectHandlers.stack.splice(0, 0, {
-            handle: function (req, res, next) {
-                var auth = require("basic-auth"),
-                    valid = Meteor.settings.basicAuth,
-                    user = auth(req);
+        // Add handler
+        WebApp.connectHandlers.use(function (req, res, next) {
+            var auth = Npm.require("basic-auth"),
+                valid = Meteor.settings.basicAuth,
+                user = auth(req);
 
-                if (!user || user.name !== valid.name || user.pass !== valid.pass) {
-                    res.statusCode = 401;
-                    res.end('Access denied');
-                } else {
-                    res.end('Access granted');
-                    next();
-                }
+            if (!user || user.name !== valid.name || user.pass !== valid.pass) {
+                res.statusCode = 401;
+                res.end('Access denied');
+            } else {
+                next();
             }
         });
+
+        // Move handler to the top of the stack
+        stack = WebApp.connectHandlers.stack;
+        stack.unshift(stack.pop());
     }
 });
