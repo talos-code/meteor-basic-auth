28626f000c9fd796b5ac0e79cbfd8c441c86690e
Rami Jarrar <rami@talos.email>
2016-02-06 23:56:54 +1100

    Bump version to 1.0.1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'talos:basic-auth',
-  version: '1.0.0',
+  version: '1.0.1',
   summary: 'Restrict Meteor deployments with HTTP Basic Auth',
   git: 'https://github.com/talos-code/meteor-basic-auth'
 });
