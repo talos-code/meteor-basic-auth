1eeffe7d3ff2d04f57c23676a35a43a2e9fea856
Rami Jarrar <rami@talos.email>
2016-02-06 22:31:22 +1100

    Implement basic auth handler

---
+++ basic-auth.js
@@ -0,0 +1,26 @@
+Meteor.startup(function () {
+    if (process.env.RESTRICT_ENV) {
+
+        // Ensure credentials are provided
+        if (!Meteor.settings.basicAuth) {
+            throw new Error("[talos:basic-auth] No credentials were found in your meteor settings");
+        }
+
+        // Add connect handler at the top of the stack
+        WebApp.connectHandlers.stack.splice(0, 0, {
+            handle: function (req, res, next) {
+                var auth = require("basic-auth"),
+                    valid = Meteor.settings.basicAuth,
+                    user = auth(req);
+
+                if (!user || user.name !== valid.name || user.pass !== valid.pass) {
+                    res.statusCode = 401;
+                    res.end('Access denied');
+                } else {
+                    res.end('Access granted');
+                    next();
+                }
+            }
+        });
+    }
+});
--- package.js
+++ package.js
@@ -4,3 +4,13 @@ Package.describe({
   summary: 'Restrict Meteor deployments with Basic Auth security ',
   git: 'https://github.com/talos-code/meteor-basic-auth'
 });
+
+Npm.depends({
+  'basic-auth': '1.0.3'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.1');
+  api.use('ecmascript');
+  api.addFiles('basic-auth.js', 'server');
+});
