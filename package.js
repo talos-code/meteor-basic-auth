83d834fbc66c9149bb839faa1035edc681068aa9
Rami Jarrar <rami@talos.email>
2016-02-06 20:43:34 +1100

    Add basic package description

---
+++ package.js
@@ -0,0 +1,6 @@
+Package.describe({
+  name: 'talos:basic-auth',
+  version: '0.0.1',
+  summary: 'Restrict Meteor deployments with Basic Auth security ',
+  git: 'https://github.com/talos-code/meteor-basic-auth'
+});
